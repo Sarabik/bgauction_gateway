0f02f322c7248feca0e5805bb2c8e40aa7c0c079
Lidija Silantjeva <lidija.silantjeva@accenture.com>
2024-10-28 17:36:56 +0200

    API Gateway created

---
+++ src/main/java/com/bgauction/gateway/config/GatewayConfig.java
@@ -0,0 +1,27 @@
+package com.bgauction.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth", r -> r.path("/auth/**")
+                        .uri("lb://USERSERVICE"))
+                .route("user_service", r -> r.path("/user/**")
+                        .uri("lb://USERSERVICE"))
+                .route("game_service", r -> r.path("/game/**")
+                        .uri("lb://GAMESERVICE"))
+                .route("auction_service", r -> r.path("/auction/**")
+                        .uri("lb://AUCTIONSERVICE"))
+                .route("bid_service", r -> r.path("/bid/**")
+                        .uri("lb://BIDSERVICE"))
+                .build();
+    }
+
+}
